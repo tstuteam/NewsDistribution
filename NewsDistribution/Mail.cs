4b2c722f89a730680838c660f2ead4b028534a65
andreymlv <ndrmlv@ya.ru>
2022-03-21 14:35:04 +0300

    added some server part

--- NewsDistribution/Mail.cs
+++ NewsDistribution/Mail.cs
@@ -8,11 +8,20 @@ public class Mail : ITarget, ISource
     private readonly List<News> _news = new();
     private readonly List<ITarget> _subscribers = new();
 
-    public void RegisterTarget(ITarget target) => _subscribers.Add(target);
+    public void RegisterTarget(ITarget target)
+    {
+        _subscribers.Add(target);
+    }
 
-    public void RemoveTarget(ITarget target) => _subscribers.Remove(target);
+    public void RemoveTarget(ITarget target)
+    {
+        _subscribers.Remove(target);
+    }
 
-    public void NotifyTargets() => _subscribers.ForEach(x => x.Update(_news.Last()));
+    public void NotifyTargets()
+    {
+        _subscribers.ForEach(x => x.Update(_news.Last()));
+    }
 
     public void Update(News news)
     {
--- ServerTCPWpfApp/MainWindow.xaml.cs
+++ ServerTCPWpfApp/MainWindow.xaml.cs
@@ -1,28 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace ServerTCPWpfApp
+namespace ServerTCPWpfApp;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private const string Ip = "127.0.0.1";
+    private const int Port = 8910;
+    private const int MaxConnections = 16;
+    private const int BufferSize = 256;
+    private IPEndPoint? _iPEndPoint;
+    private Socket? _listenSocket;
+
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    ~MainWindow()
+    {
+        _listenSocket?.Close();
+    }
+
+    private void sendButton_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void enableButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
         {
-            InitializeComponent();
+            _iPEndPoint = new IPEndPoint(IPAddress.Parse(Ip), Port);
+            _listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            _listenSocket.Bind(_iPEndPoint);
+            _listenSocket.Listen(MaxConnections);
+
+            statusLabel.Content = "Server started. Waiting for connections.";
+
+            while (true)
+            {
+                var handler = _listenSocket.Accept();
+                var builder = new StringBuilder();
+                var data = new byte[BufferSize];
+
+                do
+                {
+                    var bytes = handler.Receive(data);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                } while (handler.Available > 0);
+
+                switch (builder.ToString())
+                {
+                    case "Subscribe":
+                        break;
+                    case "Unsubscribe":
+                        break;
+                }
+                
+                const string message = "Message has been delivered.";
+                data = Encoding.Unicode.GetBytes(message);
+                handler.Send(data);
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
         }
+        catch (Exception exception)
+        {
+            statusLabel.Content = exception.ToString();
+            MessageBox.Show(exception.ToString());
+        }
+    }
+
+    private void disableButton_Click(object sender, RoutedEventArgs e)
+    {
+        _listenSocket?.Close();
+        statusLabel.Content = "The server is stopped.";
     }
 }
